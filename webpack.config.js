2f681fa95097655600757c1582b464a3fed79848
Rudah Amaral <19709437+rudah-amaral@users.noreply.github.com>
2022-09-16 11:22:32 -0300

    Adds basic boilerplate for future projects
    Similar to a create-react-app, but without a framework and I'll keep
    trowing some random stuff from time to time

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/assets"),
+    publicPath: "/assets",
+    filename: "bundle.js",
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            // Array of presets
+            presets: [
+              // Each preset is an array with an string with it's name and an
+              // object with it's configuration
+              [
+                "@babel/preset-env",
+                //
+                {
+                  // TODO: specify browser targets/support with a browserlist
+                  // integration
+                  // targets: {
+                  //   chrome: "31",
+                  // },
+
+                  // These settings are necessary to inject necessary polyfills
+                  useBuiltIns: "usage",
+                  corejs: "3.24",
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        // Order matters, loaders are executed from right to left
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
